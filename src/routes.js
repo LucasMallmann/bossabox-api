0a4fb6cc75fb60265b4e643e6739ba3708624d1f
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-05-15 00:01:12 -0300

    Middleware para exceptions assincronas com o Youch

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const validate = require('express-validation')
+const handle = require('express-async-handler')
 
 const ToolController = require('./controllers/ToolController')
 const toolValidation = require('./validations/Tool')
@@ -9,10 +10,14 @@ routes.get('/', (req, res, next) => {
   return res.status(200).json({ message: 'hello world' })
 })
 
-routes.post('/tools', validate(toolValidation), ToolController.store)
-routes.get('/tools', ToolController.index)
-routes.get('/tools/:id', ToolController.show)
-routes.delete('/tools/:id', ToolController.destroy)
-routes.put('/tools/:id', validate(toolValidation), ToolController.update)
+routes.post('/tools', validate(toolValidation), handle(ToolController.store))
+routes.get('/tools', handle(ToolController.index))
+routes.get('/tools/:id', handle(ToolController.show))
+routes.delete('/tools/:id', handle(ToolController.destroy))
+routes.put(
+  '/tools/:id',
+  validate(toolValidation),
+  handle(ToolController.update)
+)
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const validate = require('express-validation')
+const Youch = require('youch')
 
 const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
+    this.isDev = process.env.NODE_ENV === 'development'
     this.express = express()
     this.middlewares()
     this.routes()
     this.database()
+    this.exception()
   }
 
   middlewares () {
@@ -25,6 +29,19 @@ class App {
     mongoose.connect(uri, {
       useNewUrlParser: true,
       useCreateIndex: true
+    })
+  }
+
+  exception () {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      if (this.isDev) {
+        const youch = new Youch(err, req)
+        return res.json(await youch.toJSON())
+      }
     })
   }
 }
